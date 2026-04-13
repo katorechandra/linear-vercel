e0c62fec51cc70075164b7646eb8e8df8a2b1f9b
chandrakatore22@gmail.com <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>
2026-04-13 19:09:47 +0530

    fix: build + vercel deployment issues

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-import "font-awesome/css/font-awesome.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 ReactDOM.render(<App />, document.getElementById("root"));
